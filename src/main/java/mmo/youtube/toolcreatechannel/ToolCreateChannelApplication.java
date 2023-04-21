fa2ea76e9e5ebf5e4e1c11a00a824ce9768c2a5e
TUAN-PHAM\Admin <tuanpham31798@gmail.com>
2023-04-21 18:02:33 +0700

    hehe

---
+++ src/main/java/mmo/youtube/toolcreatechannel/ToolCreateChannelApplication.java
@@ -0,0 +1,160 @@
+package mmo.youtube.toolcreatechannel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@SpringBootApplication
+public class ToolCreateChannelApplication {
+	public static final int fiveSeconds = 5000;
+	public static final int tenSeconds = 10000;
+	public static final int threeeconds = 3000;
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		SpringApplication.run(ToolCreateChannelApplication.class, args);
+		// Khởi tạo ChromeDriver
+		System.setProperty("webdriver.chrome.driver", "chromedriver_win32\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		options.addArguments("--disable-notifications");
+		WebDriver driver = new ChromeDriver(options);
+
+		// Khai báo đường dẫn tới file chứa email và mật khẩu
+		String filePath = "gmail.txt";
+
+		// Đọc file chứa email và mật khẩu
+		BufferedReader br = new BufferedReader(new FileReader(filePath));
+		String line;
+		int k = 0;
+
+		String fileName = "gmail_error.txt";
+		List<String> data = new ArrayList<String>();
+
+		while ((line = br.readLine()) != null) {
+			String[] userInfo = line.split(" ");
+			String email = userInfo[0];
+			String password = userInfo[1];
+
+			driver.get("https://www.youtube.com/");
+			Thread.sleep(tenSeconds); // đợi 10 giây
+			
+			WebElement signIn = driver
+					.findElement(By.xpath("//a[@aria-label='Đăng nhập' or @aria-label='Sign in']"));
+			signIn.click();
+			
+			Thread.sleep(fiveSeconds); // đợi 20 giây
+			// Điền email và mật khẩu vào các phần tử input tương ứng
+			try {
+				WebElement emailInput = driver.findElement(By.xpath("//input[@type='email']"));
+				emailInput.sendKeys(email);
+				WebElement nextButton = driver.findElement(By.xpath("//div[@id='identifierNext']"));
+				nextButton.click();
+				Thread.sleep(fiveSeconds); // đợi 10 giây
+				WebElement passwordInput = driver.findElement(By.xpath("//input[@type='password']"));
+				passwordInput.sendKeys(password);
+				WebElement signInButton = driver.findElement(By.xpath("//div[@id='passwordNext']"));
+				signInButton.click();
+			} catch (Exception e) {
+				try {
+					System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
+					WebElement anotherAccount = driver.findElement(By.xpath(
+							"//div[contains(text(), 'Sử dụng một tài khoản khác') or contains(text(), 'Use another account')]"));
+					anotherAccount.click();
+					Thread.sleep(fiveSeconds); // đợi 10 giây
+					WebElement emailInput = driver.findElement(By.xpath("//input[@type='email']"));
+					emailInput.sendKeys(email);
+					WebElement nextButton = driver.findElement(By.xpath("//div[@id='identifierNext']"));
+					nextButton.click();
+					Thread.sleep(fiveSeconds); // đợi 10 giây
+					WebElement passwordInput = driver.findElement(By.xpath("//input[@type='password']"));
+					passwordInput.sendKeys(password);
+					WebElement signInButton = driver.findElement(By.xpath("//div[@id='passwordNext']"));
+					signInButton.click();
+				} catch (Exception e2) {
+					String st = email + " " + password;
+					data.add(st);
+					System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
+				}
+			}
+
+			// Đợi một chút để đảm bảo các hành động trước đó đã hoàn thành
+			Thread.sleep(fiveSeconds); // đợi 5 giây
+			try {
+				WebElement avatarBtn = driver.findElement(By.xpath("//button[@id='avatar-btn']"));
+				avatarBtn.click();
+			} catch (Exception e) {
+				String st = email + " " + password;
+				data.add(st);
+				System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
+			}
+			Thread.sleep(fiveSeconds); // đợi 5 giây
+			try {
+				WebElement subtitle = driver.findElement(By.xpath(
+						"//yt-formatted-string[contains(text(), 'Tạo kênh') or contains(text(), 'Create a channel')][@id='label']"));
+				subtitle.click();
+				Thread.sleep(tenSeconds); // đợi 5 giây
+				WebElement createChannel = driver
+						.findElement(By.xpath("//button[@aria-label='Tạo kênh' or @aria-label='Create channel']"));
+				createChannel.click();
+				Thread.sleep(tenSeconds); // đợi 10 giây
+				WebElement avatarB = driver.findElement(By.xpath("//button[@id='avatar-btn']"));
+				avatarB.click();
+				Thread.sleep(fiveSeconds); // đợi 5 giây
+				List<WebElement> elements = driver.findElements(By.xpath(
+						"//yt-formatted-string[contains(text(), 'Đăng xuất') or contains(text(), 'Sign out')][@id='label']"));
+				WebElement signout = elements.get(0);
+				JavascriptExecutor executor = (JavascriptExecutor) driver;
+				executor.executeScript("arguments[0].click();", signout);
+			} catch (Exception e) {
+				try {
+					System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
+					List<WebElement> elements = driver.findElements(By.xpath(
+							"//yt-formatted-string[contains(text(), 'Đăng xuất') or contains(text(), 'Sign out')][@id='label']"));
+					WebElement signout = elements.get(0);
+					JavascriptExecutor executor = (JavascriptExecutor) driver;
+					executor.executeScript("arguments[0].click();", signout);
+				} catch (Exception e2) {
+					String st = email + " " + password;
+					data.add(st);
+					System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
+				}
+			}
+			k = k + 1;
+			System.out.println("Email" + k + ": " + email);
+		}
+		br.close();
+		
+		Set<String> setWithoutDuplicates = new HashSet<>(data);
+        List<String> masterData = new ArrayList<>(setWithoutDuplicates);
+        
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+			for (String entry : masterData) {
+				bw.write(entry);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// Đóng trình duyệt
+		driver.quit();
+	}
+
+}
---
+++ src/test/java/mmo/youtube/toolcreatechannel/ToolCreateChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package mmo.youtube.toolcreatechannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolCreateChannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
