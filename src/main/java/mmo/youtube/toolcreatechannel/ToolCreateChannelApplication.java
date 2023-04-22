3f6eaeacb24fb5416650b1977d99909d9d9b6fe3
TUAN-PHAM\Admin <tuanpham31798@gmail.com>
2023-04-22 13:59:34 +0700

    hehe

--- src/main/java/mmo/youtube/toolcreatechannel/ToolCreateChannelApplication.java
+++ src/main/java/mmo/youtube/toolcreatechannel/ToolCreateChannelApplication.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -13,18 +14,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToolCreateChannelApplication {
 	public static final String filePath = "gmail.txt";
-	public static final int numberThreads = 10;
+	public static final int numberThreads = 5;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication.run(ToolCreateChannelApplication.class, args);
@@ -47,9 +49,10 @@ public class ToolCreateChannelApplication {
 			String[] parts = line.split(" "); // tách email và password ra từ chuỗi dữ liệu
 			String email = parts[0];
 			String password = parts[1];
+			String emailRecovery = parts[2];
 
 			// Tạo một Runnable mới với email và mật khẩu tương ứng
-			Runnable worker = new WorkerThread(email, password, options, data);
+			Runnable worker = new WorkerThread(email, password, emailRecovery, options, data);
 			executorService.execute(worker);
 		}
 		br.close();
@@ -60,16 +63,17 @@ public class ToolCreateChannelApplication {
 class WorkerThread implements Runnable {
 	private final String email;
 	private final String password;
+	private final String emailRecovery;
 	private final ChromeOptions options;
 	private final List<String> data;
-	public final int fiveSeconds = 3000;
-	public final int tenSeconds = 8000;
-	public final int threeeconds = 2500;
-	public final int numberThreads = 5;
+	public final int fiveSeconds = 5000;
+	public final int tenSeconds = 10000;
+	public final int threeeconds = 3000;
 
-	public WorkerThread(String email, String password, ChromeOptions options, List<String> data) {
+	public WorkerThread(String email, String password, String emailRecovery, ChromeOptions options, List<String> data) {
 		this.email = email;
 		this.password = password;
+		this.emailRecovery = emailRecovery;
 		this.options = options;
 		this.data = data;
 	}
@@ -77,25 +81,26 @@ class WorkerThread implements Runnable {
 	@Override
 	public void run() {
 		WebDriver driver = new ChromeDriver(options);
+		Duration duration = Duration.ofSeconds(300);
+		WebDriverWait wait = new WebDriverWait(driver, duration);
 		try {
 			driver.get("https://www.youtube.com/");
-			Thread.sleep(tenSeconds); // đợi 10 giây
 
 			try {
-				WebElement signIn = driver
-						.findElement(By.xpath("//a[@aria-label='Đăng nhập' or @aria-label='Sign in']"));
+				WebElement signIn = wait.until(ExpectedConditions
+						.presenceOfElementLocated(By.xpath("//a[@aria-label='Đăng nhập' or @aria-label='Sign in']")));
 				signIn.click();
 			} catch (Exception e) {
 				System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
-				String st = email + " " + password;
+				String st = email + " " + password + " " + emailRecovery;
 				data.add(st);
 				driver.quit();
 			}
 
-			Thread.sleep(fiveSeconds); // đợi 5 giây
 			// Điền email và mật khẩu vào các phần tử input tương ứng
 			try {
-				WebElement emailInput = driver.findElement(By.xpath("//input[@type='email']"));
+				WebElement emailInput = wait
+						.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='email']")));
 				emailInput.sendKeys(email);
 				WebElement nextButton = driver.findElement(By.xpath("//div[@id='identifierNext']"));
 				nextButton.click();
@@ -106,23 +111,45 @@ class WorkerThread implements Runnable {
 				signInButton.click();
 			} catch (Exception e) {
 				System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
-				String st = email + " " + password;
+				String st = email + " " + password + " " + emailRecovery;
 				data.add(st);
 				driver.quit();
 			}
 
-			// Đợi một chút để đảm bảo các hành động trước đó đã hoàn thành
-			Thread.sleep(fiveSeconds); // đợi 5 giây
 			try {
-				WebElement avatarBtn = driver.findElement(By.xpath("//button[@id='avatar-btn']"));
+				Thread.sleep(fiveSeconds); // đợi 5 giây
+				WebElement confirm = driver.findElement((By.xpath("//input[@id='confirm']")));
+				confirm.click();
+			} catch (Exception e) {
+				System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
+			}
+
+//			try {
+//				Thread.sleep(fiveSeconds); // đợi 5 giây
+//				WebElement confirmRecoveryEmail = driver.findElement((By.xpath(
+//						"//div[contains(text(), 'Xác nhận email khôi phục của bạn') or contains(text(), 'Confirm your recovery email')]")));
+//				confirmRecoveryEmail.click();
+//				Thread.sleep(fiveSeconds); // đợi 5 giây
+//				WebElement emailRecoveryEmail = driver.findElement(By.xpath("//input[@type='email']"));
+//				emailRecoveryEmail.sendKeys(emailRecovery);
+//				List<WebElement> nextButton = driver.findElements(By.xpath("//button[@type='button']"));
+//				nextButton.get(0).click();
+//			} catch (Exception e) {
+//				System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
+//			}
+
+			try {
+				WebElement avatarBtn = wait
+						.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@id='avatar-btn']")));
 				avatarBtn.click();
 			} catch (Exception e) {
 				System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
-				String st = email + " " + password;
+				String st = email + " " + password + " " + emailRecovery;
 				data.add(st);
 				driver.quit();
 			}
-			Thread.sleep(fiveSeconds); // đợi 5 giây
+
+			Thread.sleep(tenSeconds); // đợi 5 giây
 			try {
 				WebElement subtitle = driver.findElement(By.xpath(
 						"//yt-formatted-string[contains(text(), 'Tạo kênh') or contains(text(), 'Create a channel')][@id='label']"));
@@ -132,33 +159,15 @@ class WorkerThread implements Runnable {
 						.findElement(By.xpath("//button[@aria-label='Tạo kênh' or @aria-label='Create channel']"));
 				createChannel.click();
 				Thread.sleep(tenSeconds); // đợi 10 giây
-				WebElement avatarB = driver.findElement(By.xpath("//button[@id='avatar-btn']"));
-				avatarB.click();
-				Thread.sleep(fiveSeconds); // đợi 5 giây
-				List<WebElement> elements = driver.findElements(By.xpath(
-						"//yt-formatted-string[contains(text(), 'Đăng xuất') or contains(text(), 'Sign out')][@id='label']"));
-				WebElement signout = elements.get(0);
-				JavascriptExecutor executor = (JavascriptExecutor) driver;
-				executor.executeScript("arguments[0].click();", signout);
 			} catch (Exception e) {
-				try {
-					System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
-					List<WebElement> elements = driver.findElements(By.xpath(
-							"//yt-formatted-string[contains(text(), 'Đăng xuất') or contains(text(), 'Sign out')][@id='label']"));
-					WebElement signout = elements.get(0);
-					JavascriptExecutor executor = (JavascriptExecutor) driver;
-					executor.executeScript("arguments[0].click();", signout);
-				} catch (Exception e2) {
-					System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
-					String st = email + " " + password;
-					data.add(st);
-					driver.quit();
-				}
+				System.out.println("Error: " + e.getMessage() + ". " + e.getCause());
+				driver.quit();
 			}
+
 			System.out.println("Successfully created channel with Email: " + email);
 			driver.quit();
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		} finally {
 			Set<String> setWithoutDuplicates = new HashSet<>(data);
 			List<String> masterData = new ArrayList<>(setWithoutDuplicates);
